8f521ada6593a9d8aaa5d5356a8194abe68550e2
PONZE\PoNzE <ponza2539@hotmail.com>
2017-09-03 20:43:06 +0700

    use express show Online user

--- arp.js
+++ arp.js
@@ -1,5 +1,9 @@
+const express = require('express');
+const app = express();
+
 const {exec} = require('child_process')
 var dataGet = ""
+var online = ""
 exec('nmap -sP 192.168.1.1/24', (err,stdout,stderr) =>{
   if(err){
     //mean they have error
@@ -12,4 +16,20 @@ var indexOfuser = dataGet.lastIndexOf("(")
 var onlineUser = dataGet.slice(indexOfuser+1,indexOfuser+2)
 
 console.log(onlineUser)
+online = onlineUser
 })
+
+
+
+// Define port number as 3000
+const port = 3000;
+
+// Routes HTTP GET requests to the specified path "/" with the specified callback function
+app.get('/', function(request, response) {
+  response.send(online);
+});
+
+// Make the app listen on port 3000
+app.listen(port, function() {
+  console.log('Server listening on http://localhost:' + port);
+});
