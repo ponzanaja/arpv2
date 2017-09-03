5e61301f1bc65393760af3d0259daff92c620621
PONZE\PoNzE <ponza2539@hotmail.com>
2017-09-03 20:26:36 +0700

    Change to Nmap test function catchup alive user

--- arp.js
+++ arp.js
@@ -1,7 +1,15 @@
-var arpscanner = require('arpscan');
-arpscanner(onResult);
+const {exec} = require('child_process')
+var dataGet = ""
+exec('nmap -sP 192.168.1.1/24', (err,stdout,stderr) =>{
+  if(err){
+    //mean they have error
+    return
+  }
 
-function onResult(err, data){
-    if(err) console.log(err);
-    console.log(data);
-}
+dataGet = `${stdout}`
+
+var indexOfuser = dataGet.lastIndexOf("(")
+var onlineUser = dataGet.slice(indexOfuser+1,indexOfuser+2)
+
+console.log(onlineUser)
+})
