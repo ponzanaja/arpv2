9d1d05edfe8a82886cad8ec610fc7c202b91a6e9
PONZE\PoNzE <ponza2539@hotmail.com>
2017-10-18 13:11:14 +0700

    try

--- arp.js
+++ arp.js
@@ -5,29 +5,44 @@ const {exec} = require('child_process')
 var dataGet = ""
 var online = ""
 var ipNow = ""
-exec('/sbin/ifconfig eth0 | grep \'inet addr:\' | cut -d: -f2 | awk \'{ print $1}\'', (err,stdout,stderr) =>{
-  if(err){
-    //mean they have error
-    return
-  }
 
-  ipNow = `${stdout}`
 
-    exec('nmap -sP '+ ipNow +'/24', (err,stdout,stderr) =>{
+async function showResult(){
+  var a = await getIP();
+  var b = await getOnline(a);
+  console.log(b);
+
+}
+
+
+function getIP(){
+  exec('/sbin/ifconfig eth0 | grep \'inet addr:\' | cut -d: -f2 | awk \'{ print $1}\'', (err,stdout,stderr) =>{
       if(err){
         //mean they have error
         return
       }
+      console.log(ipNow)
+      return ipNow = `${stdout}`
 
-    dataGet = `${stdout}`
-
-    var indexOfuser = dataGet.lastIndexOf("(")
-    var onlineUser = dataGet.slice(indexOfuser+1,indexOfuser+2)
-
-    console.log(onlineUser)
-    online = onlineUser
     })
+}
+
+function getOnline(ip){
+  exec('nmap -sP '+ ip +'/24', (err,stdout,stderr) =>{
+    if(err){
+      //mean they have error
+      return
+    }
+
+  dataGet = `${stdout}`
+
+  var indexOfuser = dataGet.lastIndexOf("(")
+  var onlineUser = dataGet.slice(indexOfuser+1,indexOfuser+2)
+
+  console.log(onlineUser)
+  return online = onlineUser
   })
+}
 
 
 
@@ -38,6 +53,7 @@ const port = 3000;
 // Routes HTTP GET requests to the specified path "/" with the specified callback function
 app.get('/', function(request, response) {
   response.send(ipNow);
+  response.send("ทดสอบข้อความ")
   response.send(online);
 
 });
