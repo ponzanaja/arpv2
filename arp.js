6ec71d9c2ffd8f8a9a611265d2955553872f59d7
ponzanaja <ponza2539@hotmail.com>
2017-11-08 05:25:20 +0700

    Update Inbound / Outbound

--- arp.js
+++ arp.js
@@ -1,31 +1,31 @@
 const express = require('express')
 const firebase = require("firebase");
-
+const snmp = require('snmp-native')
 const app = express()
 /* root / root1234 */
 const {exec} = require('child_process')
 
- var config = {
+var config = {
     apiKey: "AIzaSyCjFxu7Ft4mfHp8ksLYoRkOSWeK4tRmI0w",
     authDomain: "showdowndata.firebaseapp.com",
     databaseURL: "https://showdowndata.firebaseio.com",
     projectId: "showdowndata",
     storageBucket: "showdowndata.appspot.com",
     messagingSenderId: "811451470025"
-}
-
+  }
 firebase.initializeApp(config);
-
+// get data from firebase
 var db  = firebase.database().ref('db')
 
 var dbInfo = []
+//when you have some data update
 db.on('child_added', function (snapshot) {
   var item = snapshot.val()
   item.id = snapshot.key
   dbInfo.push(item)
   console.log(dbInfo)
 })
-
+ // checking for update from firebase
 db.on('child_changed', function (snapshot) {
   var id = snapshot.key
   var sNode = dbInfo.find(info => info.id === id)
@@ -37,18 +37,22 @@ db.on('child_changed', function (snapshot) {
   sNode.speedtestUp = snapshot.val().speedtestUp
   sNode.speedtestDown = snapshot.val().speedtestDown
   sNode.utilize = snapshot.val().utilize
+
   console.log( ' CHANGE dbInfo \n ' + dbInfo)
 })
 
 var dataGet = ""
 var online = ""
 var ipNow = ""
-
+var sumInbound = 0
+var sumOutbound = 0
 setInterval(() => {
  console.log("We're Here now @ setInterval")
  showResult()
  sendtoFirebase("Node1")
+ getMIB("Node1")
 }, 10000)
+
 
  function showResult(){
   getIP().then(getOnline).then( (data) => {
@@ -58,6 +62,7 @@ setInterval(() => {
      online = onlineUser
   }).catch((error) => {console.error(error.message)} )
 }
+
 
  function getIP(){
    console.log("We're getting in IP")
@@ -81,7 +86,7 @@ setInterval(() => {
 }
 
 function sendtoFirebase(nodeName){
-  var check = dbInfo.find(info => info.node === nodeName)
+  let check = dbInfo.find(info => info.node === nodeName)
 
   if(check){
     firebase.database().ref('db/' + check.id).update({
@@ -89,7 +94,7 @@ function sendtoFirebase(nodeName){
       onlinenow: online
     })
   }else {
-    var sendData =  {
+    let sendData =  {
         node: nodeName,
         ip: ipNow,
         onlinenow: online,
@@ -98,7 +103,7 @@ function sendtoFirebase(nodeName){
         speedtestUp:0,
         speedtestDown:0,
         utilize:0
-    }
+        }
     return new Promise((resolve, reject) => {
       if(!db.push(sendData)) return reject( "Error can't send data to Firebase")
       else return resolve(db.push(sendData))
@@ -106,10 +111,151 @@ function sendtoFirebase(nodeName){
 
   }
 
-
-
 }
+//////////////////////////////////// Getting MIB /////////////////////////////////////////////
+function getMIB(){
+  let info = {} // all data will be here
+  var inbound = []
+  let deviceNetwork = new snmp.Session({ host:'192.168.1.254' })
+  //getInbound
+    deviceNetwork.getSubtree({ oid: [1, 3, 6, 1, 2, 1, 2, 2, 1, 10] }, function (err, varbinds) {
+      if (err) {
+        console.log(err)
+      } else {
+          varbinds.forEach((varbind) => {
+            let data = {
+              indexOID: varbind.oid[10],
+              inbound: parseInt(varbind.value / 1048576)
+            }
+            inbound.push(data)
+          })
+          console.log(inbound)
+      }
+  })
 
+  let outbound = []
+  deviceNetwork.getSubtree({ oid: [1, 3, 6, 1, 2, 1, 2, 2, 1, 16] }, function (err, varbinds) {
+    if (err) {
+      console.log(err)
+    } else {
+        varbinds.forEach((varbind) => {
+          let data = {
+            indexOID: varbind.oid[10],
+            outbound: parseInt(varbind.value / 1048576)
+          }
+          outbound.push(data)
+        })
+        console.log(outbound)
+    }
+})
+
+let intName = []
+let countInterface = 0
+deviceNetwork.getSubtree({ oid: [1, 3, 6, 1, 2, 1, 2, 2, 1, 2] }, function (err, varbinds) {
+  if (err) {
+    console.log(err)
+  } else {
+      varbinds.forEach((varbind) => {
+        let data = {
+          indexOID: varbind.oid[10],
+          intName: varbind.value
+        }
+        intName.push(data)
+        if((varbind.value).toString().toLowerCase().charAt(0) === 'f' ||  (varbind.value).toString().toLowerCase().charAt(0) == 'g'){
+          console.log((varbind.value).toString().toLowerCase().charAt(0))
+          countInterface++
+        }
+      })
+      console.log(intName)
+      console.log(countInterface)
+  }
+})
+
+function getMIB(nodeName){
+    let info = {} // all data will be here
+    var inbound = []
+    let deviceNetwork = new snmp.Session({ host:'192.168.1.254' })
+    //getInbound
+      deviceNetwork.getSubtree({ oid: [1, 3, 6, 1, 2, 1, 2, 2, 1, 10] }, function (err, varbinds) {
+        if (err) {
+          console.log(err)
+        } else {
+            varbinds.forEach((varbind) => {
+              let data = {
+                indexOID: varbind.oid[10],
+                inbound: parseInt(varbind.value / 1048576)
+              }
+              inbound.push(data)
+            })
+            // console.log(inbound) out commend for checking data
+        }
+    })
+
+    let outbound = []
+    deviceNetwork.getSubtree({ oid: [1, 3, 6, 1, 2, 1, 2, 2, 1, 16] }, function (err, varbinds) {
+      if (err) {
+        console.log(err)
+      } else {
+          varbinds.forEach((varbind) => {
+            let data = {
+              indexOID: varbind.oid[10],
+              outbound: parseInt(varbind.value / 1048576)
+            }
+            outbound.push(data)
+          })
+        //  console.log(outbound) out commend for checking data
+      }
+  })
+
+  let intName = []
+  let countInterface = 0
+  deviceNetwork.getSubtree({ oid: [1, 3, 6, 1, 2, 1, 2, 2, 1, 2] }, function (err, varbinds) {
+    if (err) {
+      console.log(err)
+    } else {
+        varbinds.forEach((varbind) => {
+          let data = {
+            indexOID: varbind.oid[10],
+            intName: varbind.value
+          }
+          intName.push(data)
+          if((varbind.value).toString().toLowerCase().charAt(0) === 'f' ||  (varbind.value).toString().toLowerCase().charAt(0) == 'g'){
+            countInterface++
+          }
+        })
+        //console.log(intName) out commend for checking data
+        console.log("Total interface is :" + countInterface)
+    }
+
+
+      for (var i = 0; i < countInterface; i++) {
+         sumInbound += inbound[i].inbound
+         sumOutbound += outbound[i].outbound
+       }
+
+  console.log(sumInbound)
+  })
+
+  let check = dbInfo.find(info => info.node === nodeName)
+  if(check){
+      firebase.database().ref('db/' + check.id).update({
+      inbound: sumInbound,
+      outbound: sumOutbound
+    })
+  }
+}
+const port = 3000;
+
+// Routes HTTP GET requests to the specified path "/" with the specified callback function
+app.get('/', function(request, response) {
+    response.send();
+
+});
+
+// Make the app listen on port 3000
+app.listen(port, function() {
+  console.log('Server listening on http://localhost:' + port);
+});
 
 // Define port number as 3000
 const port = 3000;
